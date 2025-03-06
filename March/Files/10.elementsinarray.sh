571bca8b01e350321c4a281add062d54045205ae
jyothishmohan <156563567+jyothishmohan@users.noreply.github.com>
2025-03-06 11:52:18 +0530

    Add files via upload

---
+++ March/Files/10.elementsinarray.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+display_array() {
+    arr=("$@")
+    echo "Array elements are: "
+    for element in "${arr[@]}"
+    do
+        echo "$element"
+    done
+}
+read -p "Enter the number of elements in the array: " num_elements
+
+
+array=()
+
+for (( i=0; i<num_elements; i++ ))
+do
+    read -p "Enter element $((i + 1)): " element
+    array+=("$element")
+done
+
+# Call the function to display the elements
+display_array "${array[@]}"
+
