ccf37cc894bbde1fb9f2569759d5cf53fd6de782
jyothishmohan <156563567+jyothishmohan@users.noreply.github.com>
2025-03-07 10:48:45 +0530

    Update 7.palindrome.sh

--- March/Files/7.palindrome.sh
+++ March/Files/7.palindrome.sh
@@ -2,19 +2,15 @@
 
 is_palindrome() {
     str=$1
-    len=${#str}
-
-    for (( i=0; i<len/2; i++ ))
-    do
-        if [ "${str:i:1}" != "${str:len-i-1:1}" ]; then
-            echo "The string '$str' is NOT a palindrome."
-            return
-        fi
-    done
-    echo "The string '$str' IS a palindrome."
+    reversed_str=$(echo $str | rev)
+    
+    if [ "$str" == "$reversed_str" ]; then
+        echo "The string '$str' IS a palindrome."
+    else
+        echo "The string '$str' is NOT a palindrome."
+    fi
 }
 
 read -p "Enter a string: " input_str
 
 is_palindrome $input_str
-
