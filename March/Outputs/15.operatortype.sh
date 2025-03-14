6a5b6527176ba1e441b7f19d9812c34b225f53f5
jyothishmohan <156563567+jyothishmohan@users.noreply.github.com>
2025-03-14 10:41:39 +0530

    Add files via upload

---
+++ March/Outputs/15.operatortype.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "enter first number : "
+read num1
+
+echo "enter seconf number: "
+read num2
+
+echo "enter opeartions: +,-,*,/: "
+read operation
+
+case $operation in
+        +)
+                result=$((num1 + num2))
+                echo "result:  $num1 + $num2 = $result";;
+        -)
+                result=$((num1 - num2))
+                echo "result:  $num1 - $num2 = $result";;
+        \*)
+                result=$((num1 * num2))
+                echo "result:  $num1 * $num2 = $result";;
+        /)
+                result=$((num1 / num2))
+                echo "result:  $num1 / $num2 = $result";;
+        *)
+                echo "invalid operation .....";;
+esac
+
